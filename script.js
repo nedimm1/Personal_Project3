81e9c262541f4ed10664363d36fb344bd2e0da80
github_username <nedimmuminovic61@gmail.com>
2024-02-13 14:46:23 +0100

    adding tasks

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello world')
+'use strict'
+
+
+const addTaskButton = document.querySelector('.plus-button');
+const taskBlock = document.getElementById('body');
+
+function addTask(parent, html) {
+  const task = document.createElement('div');
+  task.innerHTML = html.trim();
+  parent.appendChild(task);
+}
+
+addTaskButton.addEventListener('click', function() {
+  addTask(taskBlock, '<div id="task-block">' +  '<h2>Task</h2>' +
+    '<button class="action-button done-button">Done</button>' +
+    '<button class="action-button delete-button">Delete</button>' + "</div>");
+});
